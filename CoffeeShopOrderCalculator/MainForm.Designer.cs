a73f6f6a68c01a6eaea0fe21518f0c1ec037e8bf
isaa012or02 <isaa012or02@gmail.com>
2025-03-30 22:59:15 +0500

    first init

---
+++ CoffeeShopOrderCalculator/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CoffeeShopOrderCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.productsPanel = new System.Windows.Forms.Panel();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.Location = new System.Drawing.Point(20, 20);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(200, 26);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Меню кофейни";
+            // 
+            // productsPanel
+            // 
+            this.productsPanel.AutoScroll = true;
+            this.productsPanel.Location = new System.Drawing.Point(20, 60);
+            this.productsPanel.Name = "productsPanel";
+            this.productsPanel.Size = new System.Drawing.Size(400, 200);
+            this.productsPanel.TabIndex = 1;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(20, 280);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(200, 30);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Рассчитать стоимость";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.totalLabel.Location = new System.Drawing.Point(20, 330);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(178, 20);
+            this.totalLabel.TabIndex = 3;
+            this.totalLabel.Text = "Итоговая сумма: 0 тг.";
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(20, 370);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(200, 30);
+            this.exportButton.TabIndex = 4;
+            this.exportButton.Text = "Выгрузить заказы в Excel";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 431);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.productsPanel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Кофейня - Сбор заказов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Panel productsPanel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button exportButton;
+    }
+}
---
+++ CoffeeShopOrderCalculator/MainForm.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using System.Linq;
+
+namespace CoffeeShopOrderCalculator
+{
+    public partial class MainForm : Form
+    {
+        private SQLiteConnection connection;
+        private string dbPath = "CoffeeShopOrders.db";
+
+        // Define products and prices
+        private Dictionary<string, decimal> products = new Dictionary<string, decimal>
+        {
+            { "Кофе", 1000 },
+            { "Чай", 800 },
+            { "Круассан", 1200 },
+            { "Сок", 900 }
+        };
+
+        // Dictionary to store NumericUpDown controls for each product
+        private Dictionary<string, NumericUpDown> productQuantities = new Dictionary<string, NumericUpDown>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+
+            // Теперь создаем элементы интерфейса для продуктов программно
+            PopulateProductList();
+        }
+
+        private void PopulateProductList()
+        {
+            int yPos = 10;
+
+            // Create a header row
+            Label nameHeader = new Label
+            {
+                Text = "Продукт",
+                Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold),
+                AutoSize = true,
+                Location = new System.Drawing.Point(10, yPos)
+            };
+            productsPanel.Controls.Add(nameHeader);
+
+            Label priceHeader = new Label
+            {
+                Text = "Цена",
+                Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold),
+                AutoSize = true,
+                Location = new System.Drawing.Point(150, yPos)
+            };
+            productsPanel.Controls.Add(priceHeader);
+
+            Label quantityHeader = new Label
+            {
+                Text = "Количество",
+                Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold),
+                AutoSize = true,
+                Location = new System.Drawing.Point(250, yPos)
+            };
+            productsPanel.Controls.Add(quantityHeader);
+
+            yPos += 30;
+
+            // Add each product with its price and quantity selector
+            foreach (var product in products)
+            {
+                // Product name
+                Label nameLabel = new Label
+                {
+                    Text = product.Key,
+                    AutoSize = true,
+                    Location = new System.Drawing.Point(10, yPos)
+                };
+                productsPanel.Controls.Add(nameLabel);
+
+                // Product price
+                Label priceLabel = new Label
+                {
+                    Text = product.Value.ToString() + " тг.",
+                    AutoSize = true,
+                    Location = new System.Drawing.Point(150, yPos)
+                };
+                productsPanel.Controls.Add(priceLabel);
+
+                // Quantity numeric updown
+                NumericUpDown quantityUpDown = new NumericUpDown
+                {
+                    Minimum = 0,
+                    Maximum = 100,
+                    Value = 0,
+                    Location = new System.Drawing.Point(250, yPos),
+                    Width = 60
+                };
+                productsPanel.Controls.Add(quantityUpDown);
+
+                // Store the quantity control reference
+                productQuantities[product.Key] = quantityUpDown;
+
+                yPos += 35;
+            }
+        }
+
+        private void InitializeDatabase()
+        {
+            // Create database if it doesn't exist
+            if (!File.Exists(dbPath))
+            {
+                SQLiteConnection.CreateFile(dbPath);
+            }
+
+            // Open connection
+            connection = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+            connection.Open();
+
+            // Create table if it doesn't exist
+            string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS Orders (
+                    OrderId INTEGER PRIMARY KEY AUTOINCREMENT,
+                    OrderDetails TEXT NOT NULL,
+                    TotalAmount DECIMAL(10, 2) NOT NULL,
+                    OrderDate DATETIME NOT NULL
+                );";
+
+            using (SQLiteCommand command = new SQLiteCommand(createTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal totalAmount = 0;
+            bool hasSelectedProducts = false;
+            List<string> orderDetailsList = new List<string>();
+
+            // Calculate the total and check if at least one product is selected
+            foreach (var product in products)
+            {
+                int quantity = (int)productQuantities[product.Key].Value;
+
+                if (quantity > 0)
+                {
+                    hasSelectedProducts = true;
+                    decimal itemTotal = product.Value * quantity;
+                    totalAmount += itemTotal;
+                    orderDetailsList.Add($"{product.Key}: {quantity}");
+                }
+            }
+
+            if (!hasSelectedProducts)
+            {
+                MessageBox.Show("Выберите хотя бы один товар!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                totalLabel.Text = "Итоговая сумма: 0 тг.";
+                return;
+            }
+
+            totalLabel.Text = $"Итоговая сумма: {totalAmount} тг.";
+
+            // Save order to database
+            SaveOrderToDatabase(string.Join(", ", orderDetailsList), totalAmount);
+        }
+
+        private void SaveOrderToDatabase(string orderDetails, decimal totalAmount)
+        {
+            string insertQuery = @"
+                INSERT INTO Orders (OrderDetails, TotalAmount, OrderDate)
+                VALUES (@OrderDetails, @TotalAmount, @OrderDate);";
+
+            using (SQLiteCommand command = new SQLiteCommand(insertQuery, connection))
+            {
+                command.Parameters.AddWithValue("@OrderDetails", orderDetails);
+                command.Parameters.AddWithValue("@TotalAmount", totalAmount);
+                command.Parameters.AddWithValue("@OrderDate", DateTime.Now);
+
+                command.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Заказ успешно сохранен!", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            ExportOrdersToExcel();
+        }
+
+        private void ExportOrdersToExcel()
+        {
+            try
+            {
+                // Get all orders from database
+                DataTable ordersTable = new DataTable();
+                string selectQuery = "SELECT * FROM Orders ORDER BY OrderId;";
+
+                using (SQLiteCommand command = new SQLiteCommand(selectQuery, connection))
+                {
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(command))
+                    {
+                        adapter.Fill(ordersTable);
+                    }
+                }
+
+                if (ordersTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("Нет заказов для экспорта!", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                // ExcelPackage.LicenseContext required for EPPlus version 5+
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+                // Save file dialog
+                SaveFileDialog saveDialog = new SaveFileDialog
+                {
+                    Filter = "Excel Files (*.xlsx)|*.xlsx",
+                    Title = "Сохранить заказы",
+                    FileName = "Заказы_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")
+                };
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (ExcelPackage package = new ExcelPackage())
+                    {
+                        ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Заказы");
+
+                        // Add headers
+                        worksheet.Cells[1, 1].Value = "Номер заказа";
+                        worksheet.Cells[1, 2].Value = "Список товаров";
+                        worksheet.Cells[1, 3].Value = "Сумма (тг.)";
+                        worksheet.Cells[1, 4].Value = "Дата и время";
+
+                        // Style headers
+                        using (var range = worksheet.Cells[1, 1, 1, 4])
+                        {
+                            range.Style.Font.Bold = true;
+                        }
+
+                        // Add data
+                        for (int i = 0; i < ordersTable.Rows.Count; i++)
+                        {
+                            worksheet.Cells[i + 2, 1].Value = ordersTable.Rows[i]["OrderId"];
+                            worksheet.Cells[i + 2, 2].Value = ordersTable.Rows[i]["OrderDetails"];
+                            worksheet.Cells[i + 2, 3].Value = ordersTable.Rows[i]["TotalAmount"];
+                            worksheet.Cells[i + 2, 4].Value = Convert.ToDateTime(ordersTable.Rows[i]["OrderDate"]);
+                        }
+
+                        // Auto-fit columns
+                        worksheet.Cells.AutoFitColumns();
+
+                        // Save the file
+                        package.SaveAs(new FileInfo(saveDialog.FileName));
+                    }
+
+                    MessageBox.Show($"Заказы успешно экспортированы в файл:\n{saveDialog.FileName}",
+                                   "Экспорт завершен",
+                                   MessageBoxButtons.OK,
+                                   MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при экспорте в Excel: {ex.Message}",
+                               "Ошибка",
+                               MessageBoxButtons.OK,
+                               MessageBoxIcon.Error);
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            // Close the database connection when the form is closing
+            if (connection != null && connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+    }
+}
